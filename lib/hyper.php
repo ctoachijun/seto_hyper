917d064896682a66253f3401bddcaaa307b57b5c
ctoachijun <ctoachijun@naver.com>
2023-06-19 14:35:35 +0900

    테스트 환경 갖추고, 로그인 기능 테스트 중. 기능은 완료.

---
+++ lib/hyper.php
@@ -0,0 +1,56 @@
+<?php
+$root_path = $_SERVER['DOCUMENT_ROOT'];
+require_once $root_path."/lib/db_config.php";
+
+$host = $_SERVER["SERVER_NAME"];
+
+ini_set("session.use_trans_sid", 0);    // PHPSESSID를 자동으로 넘기지 않음
+ini_set("url_rewriter.tags","");  // 링크에 PHPSESSID가 따라다니는것을 무력화함 (해뜰녘님께서 알려주셨습니다.)
+ini_set("session.cache_expire", 180); // 세션 캐쉬 보관시간 (분)
+ini_set("session.gc_maxlifetime", 10800); // session data의 garbage collection 존재 기간을 지정 (초)
+ini_set("session.gc_probability", 1); // session.gc_probability는 session.gc_divisor와 연계하여 gc(쓰레기 수거) 루틴의 시작 확률을 관리합니다. 기본값은 1입니다. 자세한 내용은 session.gc_divisor를 참고하십시오.
+ini_set("session.gc_divisor", 100); // session.gc_divisor는 session.gc_probability와 결합하여 각 세션 초기화 시에 gc(쓰레기 수거) 프로세스를 시작할 확률을 정의합니다. 확률은 gc_probability/gc_divisor를 사용하여 계산합니다. 즉, 1/100은 각 요청시에 GC 프로세스를 시작할 확률이 1%입니다. session.gc_divisor의 기본값은 100입니다.
+ini_set("session.cookie_lifetime",0);
+
+// ini_set("session.cookie_domain",$host);
+ini_set('display_errors', 0);
+// ini_set('display_errors', 1);
+// ini_set('error_reporting', E_ALL);
+
+session_start();
+//==========================================================================================================================
+// extract($_GET); 명령으로 인해 page.php?_POST[var1]=data1&_POST[var2]=data2 와 같은 코드가 _POST 변수로 사용되는 것을 막음
+// 081029 : letsgolee 님께서 도움 주셨습니다.
+//--------------------------------------------------------------------------------------------------------------------------
+$ext_arr = array ('PHP_SELF', '_ENV', '_GET', '_POST', '_FILES', '_SERVER', '_COOKIE', '_SESSION', '_REQUEST',
+                  'HTTP_ENV_VARS', 'HTTP_GET_VARS', 'HTTP_POST_VARS', 'HTTP_POST_FILES', 'HTTP_SERVER_VARS',
+                  'HTTP_COOKIE_VARS', 'HTTP_SESSION_VARS', 'GLOBALS');
+$ext_cnt = count($ext_arr);
+for ($i=0; $i<$ext_cnt; $i++) {
+    // POST, GET 으로 선언된 전역변수가 있다면 unset() 시킴
+    if (isset($_GET[$ext_arr[$i]]))  unset($_GET[$ext_arr[$i]]);
+    if (isset($_POST[$ext_arr[$i]])) unset($_POST[$ext_arr[$i]]);
+}
+
+// PHP 4.1.0 부터 지원됨
+// php.ini 의 register_globals=off 일 경우
+@extract($_GET);
+@extract($_POST);
+@extract($_SERVER);
+
+
+
+/* 
+    관리자 관련 함수들
+*/
+function getAdminInfo($id){
+  $sql = "SELECT * FROM st_admin WHERE a_id = '{$id}'";
+  
+  return sql_fetch($sql);
+}
+
+
+
+
+?>
+
