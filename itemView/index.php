86569745b05154d99d860c02370642104a65e58b
ctoachijun <ctoachijun@naver.com>
2023-08-14 18:02:10 +0900

    23.08.14(1) - back 1. 계정 삭제 시... 로그인 체크할때 없는 계정으로 처리하는거 추가. 2. 진행상황의 날짜에 따른 페이지 구분처리를 함수로 별도 처리.(구분 방식에 변화를 줌) 3. 날짜에따라 판단하는 기준이 너무 복잡해서 머리 잡는중. (작업중임)

--- itemView/index.php
+++ itemView/index.php
@@ -10,38 +10,16 @@ $nowp = $step['is_now_page'];
 $today = date("Y-m-d");
 $now_step = $nowp;
 
+// $box1 = explode("|",$step['is_landing_date']);
+// $box2 = explode("|",$step['is_open_date']);
+// $box3 = explode("|",$step['is_pre_date']);
+
+
 if($nowp == "N"){
-  $box1 = explode("|",$step['is_landing_date']);
-  $box2 = explode("|",$step['is_open_date']);
-  $box3 = explode("|",$step['is_pre_date']);
   
-  // 각 종료일을 기준으로 우선 단계를 파악.
-  if(!empty($box1[1]) && $today <= $box1[1]){
-    $now_step = "L";
-    // echo "랜딩이다! <br>";
-  }else if(!empty($box2[1]) && $today <= $box2[1]){
-    $now_step = "O";
-    // echo "펀딩중이다!!!<br>";
-  }else if(!empty($box3[1] && $today <= $box3[1])){
-    $now_step = "P";
-    // echo "프리오더다!!! <br>";
-  }else{
-    //종료일 기준으로 단계가 없을때 시작일 기준으로 단계를 파악
-    if(!empty($box3[0]) && $today >= $box3[0]){
-      $now_step = "P";
-      // echo "프리오더당!!! <br>";
-    }else if(!empty($box2[0]) && $today >= $box2[0]){
-      $now_step = "O";
-      // echo "펀딩중이당!!!<br>";
-    }else if(!empty($box1[0] && $today >= $box1[0])){
-      $now_step = "L";
-      // echo "랜딩이당! <br>";
-    }else{
-      $now_step = "N";
-      // echo "암것도 없다아~!!!<br>";
-    }
-  }
-  
+  //시작일과 종료일별로 현재 진행단계를 파악
+  $now_step = judStepDate($step['is_landing_date'],$step['is_open_date'],$step['is_pre_date']);
+   
 }else if($nowp == "L"){
   // echo "랜딩 즉시적용이다아~!!!<br>";
 }else if($nowp == "O"){
@@ -50,12 +28,14 @@ if($nowp == "N"){
   // echo "프리오더 즉시 !!!<br>";
 }
 
+
 // 일자도, 즉시적용도 아무것도 없는 상태
 // 3단계중 그 어떤 페이지도 출력 할 수 없는상태. 안내페이지 하나 만들어서 거기로 연결하자.
 if($now_step == "N"){
   echo "즉시적용도 없고, 일자 입력도 없어서 랜딩, 오픈, 프리오더 어느쪽도 표시 할 수가 없습니다.<br>";
   echo "상품 설정을 확인 해 주세요.";
 }else{
+  
   // include 경로 추출
   $item = getItemInfo($iidx);
   $landing_url = $item['i_landing_url'];
